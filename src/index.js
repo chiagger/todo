757ad433865edc1705337038a5cafff313647ea9
chiagger <busca.chiara.cb@gmail.com>
2022-10-31 15:22:36 +0100

    sidebar logic added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 console.log("try");
+
+const openBtn = document.querySelector(".openbtn");
+const closeBtn = document.querySelector(".closebtn");
+const maincontent = document.querySelector("#maincontent");
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    const main = document.getElementById("main");
+    main.style.marginLeft = "250px";
+    main.removeChild(openBtn);
+    maincontent.style.marginLeft="250px";
+    
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    const main = document.getElementById("main");
+    main.style.marginLeft = "0px";
+    maincontent.style.marginLeft="0px";
+    main.appendChild(openBtn);
+  }
+  
+  openBtn.addEventListener("click", openNav);
+  closeBtn.addEventListener("click", closeNav);
